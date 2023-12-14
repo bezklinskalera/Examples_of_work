57dbf54a6071d479e5798fbb9c1a4b32f7f349d5
bezklinskalera <114621202+bezklinskalera@users.noreply.github.com>
2023-12-14 18:30:49 +0200

    V1

---
+++ Example_Tkinter/main.py
@@ -0,0 +1,24 @@
+# Bezklinska KP6
+
+'''Напишіть клас Python Restaurant з такими атрибутами, як menu_items,
+book_table і customer_orders, і такими методами, як add_item_to_menu,
+book_tables і customer_order.
+Додайте такий функціонал:
+Додавати пункти в меню.
+Замовляти столики.
+Приймайти замовлення клієнтів.
+Друк меню.
+Бронювання столиків.
+Друк замовлення клієнтів.
+'''
+from GUI import *
+
+app = GUI()
+
+
+
+
+
+
+
+
